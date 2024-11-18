6ec9d0c66f733583bd71bcad3e964eee8838bd33
Zhakhongir Rakhmankulov <zhakhongir.r@bilimland.com>
2024-11-18 11:51:02 +0000

    [DOT-368] - fix handle outputs when additional ports are absent, also, getting private_ips info;

--- 010-computes.tf
+++ 010-computes.tf
@@ -7,6 +7,20 @@ resource "openstack_blockstorage_volume_v3" "volume_os" {
   enable_online_resize = true
 }
 
+# Create the main network port
+resource "openstack_networking_port_v2" "main_port" {
+  name               = var.ports[0].name
+  network_id         = var.ports[0].network_id
+  admin_state_up     = true
+  security_group_ids = var.ports[0].security_group_ids
+  fixed_ip {
+    subnet_id  = var.ports[0].subnet_id
+    ip_address = var.ports[0].ip_address
+  }
+  port_security_enabled = var.ports[0].port_security
+  tags                  = var.ports[0].tags == null ? [] : var.ports[0].tags
+}
+
 # Create instance
 resource "openstack_compute_instance_v2" "instance" {
   name        = var.name
@@ -25,6 +39,11 @@ resource "openstack_compute_instance_v2" "instance" {
   config_drive = true
   user_data    = var.user_data
 
+  # Attach the main port to the instance
+  network {
+    port = openstack_networking_port_v2.main_port.id
+  }
+
   # Metadata block: combines existing OpenStack metadata with custom metadata
   metadata = { for key, value in var.metadata : key => value if value != null }
 
@@ -35,39 +54,30 @@ resource "openstack_compute_instance_v2" "instance" {
     }
   }
 
-  dynamic "network" {
-    for_each = var.ports
-
-    content {
-      port = openstack_networking_port_v2.port[network.key].id
-    }
-  }
-
   tags = var.tags
 }
 
-# Create network port
-resource "openstack_networking_port_v2" "port" {
-  count = length(var.ports)
+# Create additional ports if provided
+resource "openstack_networking_port_v2" "additional_ports" {
+  count = length(var.ports) > 1 ? length(var.ports) - 1 : 0
 
-  name               = var.ports[count.index].name
-  network_id         = var.ports[count.index].network_id
-  admin_state_up     = var.ports[count.index].admin_state_up == null ? true : var.ports[count.index].admin_state_up
-  security_group_ids = var.ports[count.index].security_group_ids == null ? [] : var.ports[count.index].security_group_ids
+  name               = var.ports[count.index + 1].name
+  network_id         = var.ports[count.index + 1].network_id
+  admin_state_up     = true
+  security_group_ids = var.ports[count.index + 1].security_group_ids
   fixed_ip {
-    subnet_id  = var.ports[count.index].subnet_id
-    ip_address = var.ports[count.index].ip_address == null ? null : var.ports[count.index].ip_address
+    subnet_id  = var.ports[count.index + 1].subnet_id
+    ip_address = var.ports[count.index + 1].ip_address
   }
-  port_security_enabled = var.ports[count.index].port_security
-  tags                  = var.ports[count.index].tags == null ? [] : var.ports[count.index].tags
+  port_security_enabled = var.ports[count.index + 1].port_security
+  tags                  = var.ports[count.index + 1].tags == null ? [] : var.ports[count.index + 1].tags
+}
 
-  dynamic "allowed_address_pairs" {
-    for_each = var.ports[count.index].allowed_address_pairs
-    content {
-      ip_address  = allowed_address_pairs.value.ip_address
-      mac_address = lookup(allowed_address_pairs.value, "mac_address", null)
-    }
-  }
+# Attach additional ports to the instance
+resource "openstack_compute_interface_attach_v2" "attached_ports" {
+  count       = length(var.ports) > 1 ? length(var.ports) - 1 : 0
+  instance_id = openstack_compute_instance_v2.instance.id
+  port_id     = openstack_networking_port_v2.additional_ports[count.index].id
 }
 
 # Create floating IP
@@ -77,10 +87,9 @@ resource "openstack_networking_floatingip_v2" "ip" {
   pool = var.public_ip_network
 }
 
-# Attach floating IP to port
+# Attach floating IP to the first additional port
 resource "openstack_networking_floatingip_associate_v2" "ipa" {
-  count = var.public_ip_network == null ? 0 : 1
-
+  count       = var.public_ip_network == null ? 0 : 1
   floating_ip = openstack_networking_floatingip_v2.ip[count.index].address
-  port_id     = openstack_networking_port_v2.port[count.index].id
+  port_id     = length(openstack_networking_port_v2.additional_ports) > 0 ? openstack_networking_port_v2.additional_ports[0].id : null
 }
--- 999-outputs.tf
+++ 999-outputs.tf
@@ -6,8 +6,11 @@ output "instance_ids" {
 
 # Output for private IPs as a flat list
 output "private_ips" {
-  value       = [for port in openstack_networking_port_v2.port : port.fixed_ip[0].ip_address]
-  description = "Flat list of private IP addresses for each port."
+  value = concat(
+    [openstack_networking_port_v2.main_port.fixed_ip[0].ip_address],
+    [for port in openstack_networking_port_v2.additional_ports : port.fixed_ip[0].ip_address]
+  )
+  description = "Flat list of private IP addresses for all ports (main and additional)."
 }
 
 # Output for floating IPs as a flat list
@@ -18,13 +21,51 @@ output "floating_ips" {
 
 # Output for ports with details as a flat list
 output "instance_info" {
+  value = concat(
+    [
+      {
+        id         = openstack_networking_port_v2.main_port.id
+        name       = openstack_networking_port_v2.main_port.name
+        tags       = openstack_networking_port_v2.main_port.tags
+        private_ip = openstack_networking_port_v2.main_port.fixed_ip[0].ip_address
+      }
+    ],
+    [
+      for port in length(openstack_networking_port_v2.additional_ports) > 0 ? openstack_networking_port_v2.additional_ports : [] : {
+        id         = port.id
+        name       = port.name
+        tags       = port.tags
+        private_ip = port.fixed_ip[0].ip_address
+      }
+    ]
+  )
+  description = "Flat list of ports with tags, names, and private IPs for the instance."
+}
+
+# Output for the primary port details
+output "primary_port" {
+  value = {
+    id         = openstack_networking_port_v2.main_port.id
+    name       = openstack_networking_port_v2.main_port.name
+    private_ip = openstack_networking_port_v2.main_port.fixed_ip[0].ip_address
+  }
+  description = "Details of the primary port attached to the instance."
+}
+
+# Output for additional ports
+output "additional_ports" {
   value = [
-    for port in openstack_networking_port_v2.port : {
+    for port in openstack_networking_port_v2.additional_ports : {
       id         = port.id
       name       = port.name
-      tags       = port.tags
       private_ip = port.fixed_ip[0].ip_address
     }
   ]
-  description = "Flat list of ports with tags, names, and private IPs for the instance."
+  description = "Details of additional ports attached to the instance."
+}
+
+# Output for attached floating IPs
+output "attached_floating_ips" {
+  value       = [for assoc in openstack_networking_floatingip_associate_v2.ipa : assoc.floating_ip]
+  description = "List of floating IPs attached to additional ports."
 }
